8ee77f190be61d58c0629295879cbba2145f654f
COLVERTYETY <niconoob1@gmail.com>
2020-12-18 14:05:33 +0100

    pretty up

--- Boogle/superchrono.cs
+++ Boogle/superchrono.cs
@@ -5,8 +5,8 @@ namespace Boogle
 {
     public class superchrono
     {
-        private int xcursor=30;
-        private int ycursor=5;
+        private int xcursor=2;
+        private int ycursor=0;
         private Stopwatch chrono = new Stopwatch();
         private TimeSpan theduration;
         public bool isitoveryet{
@@ -20,11 +20,12 @@ namespace Boogle
             chrono.Restart();
             while(this.isitoveryet){
                 Thread.Sleep(100);
+                string empty ="Il vous reste: "+Convert.ToString((this.theduration-chrono.Elapsed).Seconds)+"s";
                 double temp = (this.theduration.TotalSeconds-this.chrono.Elapsed.TotalSeconds)/(this.theduration.TotalSeconds);
                 temp*=100;
                 int cursorleft = Console.CursorLeft;
                 int cursortop = Console.CursorTop;
-                Console.SetCursorPosition(xcursor,ycursor);
+                Console.SetCursorPosition(Console.WindowWidth - empty.Length - xcursor,ycursor);
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 if(temp<80){
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -41,12 +42,14 @@ namespace Boogle
                 if(temp<15){
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
-                Console.Write("Il vous reste: ");
-                Console.Write((this.theduration-chrono.Elapsed).Seconds);
+                Console.Write(empty);
                 Console.ResetColor();
                 Console.CursorLeft = cursorleft;
                 Console.CursorTop = cursortop;
             }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Le temps est écoulé !!!");
+            Console.ResetColor();
         }
     }
 }
